bba263bcb76aa74cc8c54140d86703a76a7c5b35
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-07-12 09:58:42 -0400

    Update goal3.py

--- pbj/goal3.py
+++ pbj/goal3.py
@@ -2,9 +2,6 @@ bread = 9
 pb = 5
 jam = 7
 
-if (bread >= 2) and (pb >= 1) and (jam >= 1):
-	print 'There\'s enough bread, peanut butter, and jam to make a sandwich!'
-
 full_sandwiches = bread / 2
 
 #print 'Based on how much bread I have, I can make {0} sandwiches.'.format(full_sandwiches)
