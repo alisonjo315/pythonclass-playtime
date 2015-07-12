352bd0bcee6818d81655f6acd62af95fde3c81cc
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-07-12 10:10:52 -0400

    Update goal2.py
    totally changed code

--- pbj/goal2.py
+++ pbj/goal2.py
@@ -6,7 +6,18 @@ jam = 7
 
 full_sandwiches = bread / 2
 
-if (bread >= 2) and (pb >= 1) and (jam >= 1):
+if (full_sandwiches < pb) and (full_sandwiches < jam):
+    least_ingr = "bread"
+    least_num = full_sandwiches
+elif (pb < full_sandwiches) and (pb < jam):
+    least_ingr = "pb"
+    least_num = pb
+else:
+    least_ingr = "jam"
+    least_num = jam
+
+if (full_sandwiches >= 1) and (pb >= 1) and (jam >= 1):
 	print 'There\'s enough bread, peanut butter, and jam to make a sandwich!'
+        print 'You can make {0} sandwich(es)!'.format(least_num)	
 else:
         print 'There\'s not enough bread and/or PB and/or jam to make a sandwich :('
