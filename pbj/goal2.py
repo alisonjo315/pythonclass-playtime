3adb8b878cc3e02826ebbe61d9be4b67a5d4126c
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-07-12 09:59:39 -0400

    Update goal2.py

--- pbj/goal2.py
+++ pbj/goal2.py
@@ -1,12 +1,10 @@
-# Create a program that can tell you whether or not you can make a peanut butter and jelly sandwich.
-
-bread = 8
+bread = 9
 pb = 5
 jam = 7
 
+full_sandwiches = bread / 2
+
 if (bread >= 2) and (pb >= 1) and (jam >= 1):
 	print 'There\'s enough bread, peanut butter, and jam to make a sandwich!'
-
-sandwiches_bread = bread / 2
-
-print 'Based on how much bread I have, I can make {numsw} sandwiches.'.format(numsw=sandwiches_bread)
+else:
+        print 'There\'s not enough bread and/or PB and/or jam to make a sandwich :('
