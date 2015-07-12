d5cf4e13e903769a9cb8b38df678f34fdda6144d
alisonjo2786 <alisonjo2786@users.noreply.github.com>
2015-07-12 10:00:23 -0400

    Update goal1.py
    Added first goal / instruction

--- pbj/goal1.py
+++ pbj/goal1.py
@@ -1,4 +1,5 @@
 # Create a program that can tell you whether or not you can make a peanut butter and jelly sandwich.
+# First Goal: Create a program that can tell you whether or not you can make a peanut butter and jelly sandwich
 
 bread = 8
 pb = 5
@@ -6,4 +7,3 @@ jam = 7
 
 if (bread >= 2) and (pb >= 1) and (jam >= 1):
 	print 'There\'s enough bread, peanut butter, and jam to make a sandwich!'
-
